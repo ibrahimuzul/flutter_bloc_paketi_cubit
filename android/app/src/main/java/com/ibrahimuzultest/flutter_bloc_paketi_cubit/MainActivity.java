ca3a3eaf5f89c11eb74f0986c916002c95529f95
iuzul <iuzul@IBRAHIMUZUL>
2020-11-06 10:59:29 +0300

    Uygulamanın tamamı, eğitimden tamamlandı.

---
+++ android/app/src/main/java/com/ibrahimuzultest/flutter_bloc_paketi_cubit/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ibrahimuzultest.flutter_bloc_paketi_cubit;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/counter_bloc/counter_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'counter_event.dart';
+import 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterState(0));
+
+  @override
+  Stream<CounterState> mapEventToState(CounterEvent event) async* {
+    if (event is CounterArttir) {
+      yield CounterState(state.sayac + 1);
+    }
+    if (event is CounterAzalt) {
+      yield CounterState(state.sayac - 1);
+      //eğer o an sayac değeri 4 ise ekrana CounterState(3) döndürülür.
+    }
+  }
+}
